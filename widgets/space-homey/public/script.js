d08c60fbd8e1e32c5b256c292bcf42b8c2cfa42e
Ludvig Aldrin <ludvig.aldrin@gmail.com>
2025-03-11 13:21:59 +0100

    update

--- widgets/space-homey/public/script.js
+++ widgets/space-homey/public/script.js
@@ -25,6 +25,9 @@ async function init() {
     // First Show Loading State
     showLoadingState();
 
+
+
+
     // Now load the widget
     widgetId = await Homey.getWidgetInstanceId();
 
@@ -33,6 +36,7 @@ async function init() {
         return;
     }
 
+    Homey.api('POST', '/log', { message: 'Init widget: ' + widgetId });
     // Get all floors
     const floors = await Homey.api('GET', '/floors');
 
@@ -284,18 +288,18 @@ function addSettingsButton() {
     const button = document.createElement('button');
     button.className = 'settings-button';
     button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>`;
-    
+
     // Change the click handler to show the floor selector
     button.addEventListener('click', async () => {
         try {
             // Get all floors
             const floors = await Homey.api('GET', '/floors');
-            
+
             if (!floors || floors.length === 0) {
                 showNoFloorsMessage();
                 return;
             }
-            
+
             // Show the floor selector
             showFloorSelector(floors);
         } catch (error) {
